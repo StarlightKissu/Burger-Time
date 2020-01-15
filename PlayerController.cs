33fc9617efbade64575d7dd5d3fc4fdcea64b389
MrInfinity <owens1427@gmail.com>
2020-01-15 12:14:47 -0500

    v1.0

---
+++ PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+    public float speed;
+
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.velocity = (new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+}
